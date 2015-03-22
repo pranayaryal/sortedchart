959b1521fadec9552fc6ecf4c3f0074571341573
pranayaryal <drpranayaryal@gmail.com>
2015-03-22 02:59:47 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var defData = [
+        {"team": "d", "cycleTime": 1, "effort": 1, "count": 1, "priority": "low"}, {
+            "team": "d",
+            "cycleTime": 2,
+            "effort": 2,
+            "count": 5,
+            "priority": "low"
+        }, {"team": "d", "cycleTime": 3, "effort": 3, "count": 8, "priority": "medium"}, {
+            "team": "d",
+            "cycleTime": 4,
+            "effort": 4,
+            "count": 3,
+            "priority": "high"
+        }, {"team": "l", "cycleTime": 2, "effort": 1, "count": 1, "priority": "low"}, {
+            "team": "l",
+            "cycleTime": 3,
+            "effort": 2,
+            "count": 5,
+            "priority": "low"
+        }, {"team": "l", "cycleTime": 4, "effort": 3, "count": 8, "priority": "medium"}, {
+            "team": "l",
+            "cycleTime": 5,
+            "effort": 4,
+            "count": 3,
+            "priority": "high"
+        },
+        {"team": "k", "cycleTime": 2, "effort": 4, "count": 1, "priority": "low"}, {
+            "team": "k",
+            "cycleTime": 3,
+            "effort": 5,
+            "count": 5,
+            "priority": "low"
+        }, {"team": "k", "cycleTime": 4, "effort": 6, "count": 8, "priority": "medium"}, {
+            "team": "k",
+            "cycleTime": 5,
+            "effort": 8,
+            "count": 3,
+            "priority": "high"
+        }];
+var chart = new tauCharts.Chart({
+            data: defData,
+            type: 'bar',           
+            x: 'team',
+            y: 'effort'
+        });
